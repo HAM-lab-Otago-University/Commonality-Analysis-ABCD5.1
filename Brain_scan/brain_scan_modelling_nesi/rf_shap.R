d3bf41d287f003a9237e996359f056e35a08f781
wylovejordan <50278047+wylovejordan@users.noreply.github.com>
2024-11-19 10:16:51 +1300

    Add files via upload

---
+++ Brain_scan/brain_scan_modelling_nesi/rf_shap.R
@@ -0,0 +1,87 @@
+## library path
+.libPaths(new = "/scale_wlg_persistent/filesets/home/wanyu368/R/foss-2023a/4.3" )
+#for parallel processing
+
+library(tidymodels)
+library(tidyverse)
+#library(future)
+#plan(multisession, workers = strtoi(Sys.getenv('SLURM_CPUS_PER_TASK')))
+#plan(multisession, workers = 15)
+library(doParallel)
+
+registerDoParallel(strtoi(Sys.getenv('SLURM_CPUS_PER_TASK')))
+
+#cl <- makePSOCKcluster(15)
+#registerDoParallel(cl)
+
+datafolder <- "/nesi/nobackup/uoo03493/Yue/stacking_gfactor_modelling/"
+#datafolder <- "/Volumes/sci-psy-narun/Nesi/Yue/stacking_gfactor_modelling/"
+
+source(paste0(datafolder,"r_functions.R"))
+
+args <- commandArgs(trailingOnly = TRUE)
+dataset <- args[1]
+#dataset <- "random_forest_followup_results.RDS"
+
+batch_idx <- strtoi(Sys.getenv('SLURM_ARRAY_TASK_ID'))
+#batch_idx <- 1
+
+#print(batch_idx)
+
+# number of threads to use for parallel xgboost
+# passed into set_engine()
+#threads <- strtoi(Sys.getenv("SLURM_CPU_PER_TASK"))
+
+print("Setup complete!")
+message("Setup complete!")
+
+list_input_all_site <- readRDS(file = paste0(datafolder, "collect_random_forest_results/", dataset))
+
+list_input <- list_input_all_site[[batch_idx]]
+
+model_fit_input <- list_input[["train_pred"]][["model_final_fit"]]
+
+#model_fit_input
+
+train_data <- list_input[["train_data"]]
+
+#train_data
+
+test_data <- list_input[["test_data"]]
+
+#test_data
+
+
+subj_info <-  c("SRC_SUBJECT_ID","EVENTNAME","SITE_ID_L")   
+
+subj_info_resp <-  c(subj_info,"gfactor")
+
+
+processed_train_data <-  train_data%>%
+  select(-all_of(subj_info_resp))%>%
+  as.data.frame()
+
+
+##need to test this works for all the models
+model_pred_fun <- function(object, newdata) {
+  pred_results <- predict(object, new_data = newdata)
+  return(pred_results$.pred)
+}  
+
+
+model_shap <- model_fit_input %>% 
+  fastshap::explain(X =processed_train_data,
+                    nsim= 1000, 
+                    pred_wrapper =model_pred_fun,
+                    parallel=TRUE
+  )
+
+
+output_list <- list(train_data = train_data,
+                    test_data = test_data,
+                    model_shap = model_shap)
+
+data_name <- gsub(dataset, pattern = "\\.RDS", replacement = "" )
+
+saveRDS(output_list, paste0(datafolder, "rf_shap_results/" ,data_name, "_shap_",batch_idx,".RDS" ))
+
